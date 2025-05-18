c6b93cd0697656afc8db0b729af9a757f4b43306
anhnt02 <anhnt24072002@gmail.com>
2025-05-18 22:47:41 +0700

    Update Latex Render Contain Text

--- lib/services/latex_renderer_service.dart
+++ lib/services/latex_renderer_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class LatexRendererService {
   final String baseUrl = 'http://192.168.92.200:5000';
 
-  Future<List<String>> renderLatexFromText(String latexText) async {
+  Future<List<Map<String, dynamic>>> renderLatexFromText(String latexText) async {
     final url = Uri.parse('$baseUrl/render_latex');
     final headers = {'Content-Type': 'application/json'};
     final body = json.encode({'latex_code': latexText});
@@ -13,7 +13,8 @@ class LatexRendererService {
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      return List<String>.from(data['images']);
+      // Trả về nguyên list segments (text và latex)
+      return List<Map<String, dynamic>>.from(data['segments']);
     } else {
       throw Exception('Failed to render LaTeX: ${response.body}');
     }
