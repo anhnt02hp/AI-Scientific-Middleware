f36e013771841aa8763ec01451bc91d94a1f2508
anhnt02 <anhnt24072002@gmail.com>
2025-07-16 23:05:12 +0700

    Add QnA in slide

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String baseURL = "https://openrouter.ai/api/v1";
-String apiKey = "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";
+String apiKey = "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";
